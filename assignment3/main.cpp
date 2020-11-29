517477d837e45f374ad0b7d68fe0f33284c1a068
Ryan Stolys <rstolys@sfu.ca>
2020-11-28 23:44:55 -0800

    final edits

--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -25,7 +25,7 @@ const int FAILURE =                 1;
 
 const int NUM_KEYS =                1000000;             //100 Thousand Keys
 const int KEY_GAP =                 8647;               //Randomish key gap to get good spread of keys
-const int MAX_KEY =                 10000000;            //1 million
+const int MAX_KEY =                 100000000;            //1 million
 
 const int SIZE =                    0; 
 const int HEIGHT =                  1;
